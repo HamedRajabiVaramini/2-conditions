0eed551916841d45ec5726be3c4b866e0f0a8d33
Hamed Rajabi Varamini <hamed.r.v@gmail.com>
2024-02-08 17:22:22 +0400

    Chore;Adds equals, greater/less than or equal conditions

---
+++ equals.py
@@ -0,0 +1,10 @@
+'''
+simple_programming_
+'''
+name = "python"
+
+if name == "c#": 
+    print("name is c#")
+
+if name != "java":
+    print("name is NOT java")
---
+++ greater_equal.py
@@ -0,0 +1,6 @@
+'''
+simple_programming_
+'''
+x = 5
+if x >= 5: 
+    print("x is greater than or equal to 5")
